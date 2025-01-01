1c22405396ee9117748974e7163d5a0ef9542227
jn-devops <devops@joy-nostalg.com>
2025-01-01 11:55:46 +0800

    add product-details end point

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\GetPropertyDetailController;
+use App\Http\Controllers\{GetProductDetailController, GetPropertyDetailController};
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
@@ -10,3 +10,6 @@ Route::get('/user', function (Request $request) {
 
 Route::get('property-details/{property_code}', GetPropertyDetailController::class)
     ->name('property-details');
+
+Route::get('product-details/{product_code}', GetProductDetailController::class)
+    ->name('product-details');
